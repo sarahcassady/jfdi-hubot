50c0d90cc82b8da07994dbe69f43cc5e10d43f09
Sarah Cassady <sarah@cohort.io>
2013-10-20 17:17:26 -0600

    rm get call

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
+app.use(express.logger()); 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
