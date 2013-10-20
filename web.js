61efb99d6c90a2694340d07a5a1ddf782dc21965
Sarah Cassady <sarah@cohort.io>
2013-10-20 16:42:59 -0600

    update .env and web.js to point to index.php

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.get('/index.php');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
