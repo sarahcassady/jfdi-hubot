9050c11b5bf794eaf38b58a0aba94d41a7dc2c24
Sarah Cassady <sarah@cohort.io>
2013-10-20 16:59:18 -0600

    removed call to get index

--- web.js
+++ web.js
@@ -2,8 +2,6 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/index.php');
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
