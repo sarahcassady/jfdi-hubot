337819a530ba34d89e42ee7ce384c9dde7e3873b
Sarah Cassady <sarah@cohort.io>
2013-10-20 23:13:32 -0600

    static pages set at /pub

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 app.use(express.logger()); 
+app.use(express.static(__dirname + '/pub'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
