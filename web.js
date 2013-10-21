144122ae86f0e9057a5b17a98154a7f64ecde450
Sarah Cassady <sarah@cohort.io>
2013-10-21 01:45:54 -0600

    update express static

--- web.js
+++ web.js
@@ -2,7 +2,9 @@ var express = require("express");
 var app = express();
 app.use(express.logger()); 
 
-app.get('/public'); //, function(request, response) { response.send('Hello World!'); });
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
