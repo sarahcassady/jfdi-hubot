8124dbefc6dd3c74f36c354ee7af08ff14e5d867
Sarah Cassady <sarah@cohort.io>
2013-10-20 23:47:15 -0600

    get calls index.html from app root

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
 app.use(express.logger()); 
-app.use(express.static(__dirname + '/pub'));
+
+app.get("/", function(req, res) {
+  res.redirect("/index.html");
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
