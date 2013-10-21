c9cc5a3133d548a8cfad88e394404344a76d797a
Sarah Cassady <sarah@cohort.io>
2013-10-21 03:14:21 -0600

    added node-static

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bin/hubot -a campfire -n hubot-jfdi
+worker: node worker.js
--- web.js
+++ web.js
@@ -2,27 +2,9 @@ var express = require("express");
 var app = express();
 app.use(express.logger()); 
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 }); 
 
-    var static = require('node-static');
-
-    //
-    // Create a node-static server instance to serve the './public' folder
-    //
-    var file = new static.Server('./public');
-
-    require('http').createServer(function (request, response) {
-        request.addListener('end', function () {
-            //
-            // Serve files!
-            //
-            file.serve(request, response);
-        }).resume();
-    }).listen(8080);
+   
