d8b4109385a0aa585b70b9a0a20e2edfec3468b3
Sarah Cassady <sarah@cohort.io>
2013-11-05 23:14:02 -0700

    static service added

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
-var express = require("express");
-var app = express();
+var 	express = require('express');
+var	app = express();
 app.use(express.logger()); 
-
-var port = process.env.PORT || 5000;
+app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-}); 
+});
 
-   
