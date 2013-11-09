0ac0dfdf26d2b54f5c8966c982424a370c7a0c3b
Sarah Cassady <sarah@cohort.io>
2013-11-08 22:36:26 -0700

    add second process; new port

--- web.js
+++ web.js
@@ -7,10 +7,10 @@ var fs = require('fs');
 var cas = [fs.readFileSync('/Users/sarahcassady/ca.pem')];
 https.globalAgent.options.ca = cas;
 */
-/*
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-*/
-app.listen(8080, "127.0.0.1");
+
+/*app.listen(8080, "127.0.0.1");*/
