ed215fb84f9224b975c08e4203004ea02ffc28c3
Sarah Cassady <sarah@cohort.io>
2013-11-08 22:11:53 -0700

    static content update

--- web.js
+++ web.js
@@ -2,7 +2,12 @@ var 	express = require('express');
 var	app = express();
 app.use(express.logger()); 
 app.use(express.static(__dirname + '/public'));
-var port = process.env.PORT || 3000;
+/*
+var fs = require('fs');
+var cas = [fs.readFileSync('/Users/sarahcassady/ca.pem')];
+https.globalAgent.options.ca = cas;
+*/
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
